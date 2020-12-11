7a1ef258611c7b029c7e58a8b0d5db9bcc9fc99c
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-12-11 19:58:29 +0100

    commit2

---
+++ IRF_Project_DCWC5L/IRF_Project_DCWC5L/Entities/CDInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project_DCWC5L.Entities
+{
+    public class CDInfo
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public int PublicationYear { get; set; }
+
+    }
+}
