6ca5fbebc36eb7524ade568323f998feb7712d6c
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-12-11 20:24:05 +0100

    commit6

--- IRF_Project_DCWC5L/IRF_Project_DCWC5L/Form1.Designer.cs
+++ IRF_Project_DCWC5L/IRF_Project_DCWC5L/Form1.Designer.cs
@@ -32,7 +32,21 @@
             this.textBoxOpenFile = new System.Windows.Forms.TextBox();
             this.buttonOpenFile = new System.Windows.Forms.Button();
             this.dataGridViewCD = new System.Windows.Forms.DataGridView();
+            this.labelFullName = new System.Windows.Forms.Label();
+            this.labelShortName = new System.Windows.Forms.Label();
+            this.labelAccount = new System.Windows.Forms.Label();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
+            this.textBoxShortName = new System.Windows.Forms.TextBox();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.buttonAddList = new System.Windows.Forms.Button();
+            this.dataGridViewPersonList = new System.Windows.Forms.DataGridView();
+            this.buttonSaveList = new System.Windows.Forms.Button();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.richTextBoxRandomCD = new System.Windows.Forms.RichTextBox();
+            this.buttonAddPresent = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // labelOpenFile
@@ -69,11 +83,122 @@
             this.dataGridViewCD.Size = new System.Drawing.Size(279, 369);
             this.dataGridViewCD.TabIndex = 3;
             // 
+            // labelFullName
+            // 
+            this.labelFullName.AutoSize = true;
+            this.labelFullName.Location = new System.Drawing.Point(352, 19);
+            this.labelFullName.Name = "labelFullName";
+            this.labelFullName.Size = new System.Drawing.Size(146, 13);
+            this.labelFullName.TabIndex = 4;
+            this.labelFullName.Text = "Megajándékozott teljes neve:";
+            // 
+            // labelShortName
+            // 
+            this.labelShortName.AutoSize = true;
+            this.labelShortName.Location = new System.Drawing.Point(352, 52);
+            this.labelShortName.Name = "labelShortName";
+            this.labelShortName.Size = new System.Drawing.Size(59, 13);
+            this.labelShortName.TabIndex = 5;
+            this.labelShortName.Text = "Beceneve:";
+            // 
+            // labelAccount
+            // 
+            this.labelAccount.AutoSize = true;
+            this.labelAccount.Location = new System.Drawing.Point(352, 79);
+            this.labelAccount.Name = "labelAccount";
+            this.labelAccount.Size = new System.Drawing.Size(128, 13);
+            this.labelAccount.TabIndex = 6;
+            this.labelAccount.Text = "iTunes accountja (e-mail):";
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Location = new System.Drawing.Point(513, 11);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFullName.TabIndex = 7;
+            // 
+            // textBoxShortName
+            // 
+            this.textBoxShortName.Location = new System.Drawing.Point(513, 45);
+            this.textBoxShortName.Name = "textBoxShortName";
+            this.textBoxShortName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxShortName.TabIndex = 8;
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(513, 72);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAccount.TabIndex = 9;
+            // 
+            // buttonAddList
+            // 
+            this.buttonAddList.Location = new System.Drawing.Point(355, 112);
+            this.buttonAddList.Name = "buttonAddList";
+            this.buttonAddList.Size = new System.Drawing.Size(258, 28);
+            this.buttonAddList.TabIndex = 10;
+            this.buttonAddList.Text = "Hozzáadás a megadjándékozottak listájához";
+            this.buttonAddList.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewPersonList
+            // 
+            this.dataGridViewPersonList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersonList.Location = new System.Drawing.Point(355, 167);
+            this.dataGridViewPersonList.Name = "dataGridViewPersonList";
+            this.dataGridViewPersonList.Size = new System.Drawing.Size(258, 256);
+            this.dataGridViewPersonList.TabIndex = 11;
+            // 
+            // buttonSaveList
+            // 
+            this.buttonSaveList.Location = new System.Drawing.Point(650, 112);
+            this.buttonSaveList.Name = "buttonSaveList";
+            this.buttonSaveList.Size = new System.Drawing.Size(119, 29);
+            this.buttonSaveList.TabIndex = 12;
+            this.buttonSaveList.Text = "Ajándéklista mentése";
+            this.buttonSaveList.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Location = new System.Drawing.Point(650, 11);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(120, 90);
+            this.pictureBoxLogo.TabIndex = 13;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // richTextBoxRandomCD
+            // 
+            this.richTextBoxRandomCD.Location = new System.Drawing.Point(650, 257);
+            this.richTextBoxRandomCD.Name = "richTextBoxRandomCD";
+            this.richTextBoxRandomCD.Size = new System.Drawing.Size(119, 166);
+            this.richTextBoxRandomCD.TabIndex = 14;
+            this.richTextBoxRandomCD.Text = "";
+            // 
+            // buttonAddPresent
+            // 
+            this.buttonAddPresent.Location = new System.Drawing.Point(650, 167);
+            this.buttonAddPresent.Name = "buttonAddPresent";
+            this.buttonAddPresent.Size = new System.Drawing.Size(119, 70);
+            this.buttonAddPresent.TabIndex = 15;
+            this.buttonAddPresent.Text = "Megajándékozom őt a kiválasztott CD-vel";
+            this.buttonAddPresent.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAddPresent);
+            this.Controls.Add(this.richTextBoxRandomCD);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.buttonSaveList);
+            this.Controls.Add(this.dataGridViewPersonList);
+            this.Controls.Add(this.buttonAddList);
+            this.Controls.Add(this.textBoxAccount);
+            this.Controls.Add(this.textBoxShortName);
+            this.Controls.Add(this.textBoxFullName);
+            this.Controls.Add(this.labelAccount);
+            this.Controls.Add(this.labelShortName);
+            this.Controls.Add(this.labelFullName);
             this.Controls.Add(this.dataGridViewCD);
             this.Controls.Add(this.buttonOpenFile);
             this.Controls.Add(this.textBoxOpenFile);
@@ -81,6 +206,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,6 +219,18 @@
         private System.Windows.Forms.TextBox textBoxOpenFile;
         private System.Windows.Forms.Button buttonOpenFile;
         private System.Windows.Forms.DataGridView dataGridViewCD;
+        private System.Windows.Forms.Label labelFullName;
+        private System.Windows.Forms.Label labelShortName;
+        private System.Windows.Forms.Label labelAccount;
+        private System.Windows.Forms.TextBox textBoxFullName;
+        private System.Windows.Forms.TextBox textBoxShortName;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.Button buttonAddList;
+        private System.Windows.Forms.DataGridView dataGridViewPersonList;
+        private System.Windows.Forms.Button buttonSaveList;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.RichTextBox richTextBoxRandomCD;
+        private System.Windows.Forms.Button buttonAddPresent;
     }
 }
 
