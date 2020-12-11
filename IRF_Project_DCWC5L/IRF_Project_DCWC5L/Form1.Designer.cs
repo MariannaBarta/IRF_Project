9401ccc40de11c07a930155c7c6f883e15aa1ada
Marianna Barta <marianna.barta@stud.uni-corvinus.hu>
2020-12-11 20:01:18 +0100

    commit3

--- IRF_Project_DCWC5L/IRF_Project_DCWC5L/Form1.Designer.cs
+++ IRF_Project_DCWC5L/IRF_Project_DCWC5L/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelOpenFile = new System.Windows.Forms.Label();
+            this.textBoxOpenFile = new System.Windows.Forms.TextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelOpenFile
+            // 
+            this.labelOpenFile.AutoSize = true;
+            this.labelOpenFile.Location = new System.Drawing.Point(24, 19);
+            this.labelOpenFile.Name = "labelOpenFile";
+            this.labelOpenFile.Size = new System.Drawing.Size(86, 13);
+            this.labelOpenFile.TabIndex = 0;
+            this.labelOpenFile.Text = "Zenei lista helye:";
+            // 
+            // textBoxOpenFile
+            // 
+            this.textBoxOpenFile.Location = new System.Drawing.Point(116, 12);
+            this.textBoxOpenFile.Name = "textBoxOpenFile";
+            this.textBoxOpenFile.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOpenFile.TabIndex = 1;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(231, 9);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 2;
+            this.buttonOpenFile.Text = "Tallózás";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 369);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.textBoxOpenFile);
+            this.Controls.Add(this.labelOpenFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelOpenFile;
+        private System.Windows.Forms.TextBox textBoxOpenFile;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
